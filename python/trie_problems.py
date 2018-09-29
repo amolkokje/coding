0bd70e2bc40dbd25f30d3ac685b461f5ca0946b4
Amol Kokje <amol_kokje@mcafee.com>
2018-09-29 11:30:23 -0700

    fixed

--- python/trie_problems.py
+++ python/trie_problems.py
@@ -41,9 +41,11 @@ class Trie:
     def __init__(self):
         self.root = self.getNode()
  
+ 
     def getNode(self):     
         # Returns new trie node (initialized to NULLs)
         return TrieNode()
+ 
  
     def _charToIndex(self, ch):         
         """
@@ -72,6 +74,7 @@ class Trie:
         # mark last node as leaf
         pCrawl.isEndOfWord = True
  
+ 
     def search(self, key):
         """ 
         Search key in the trie
@@ -89,51 +92,28 @@ class Trie:
         return pCrawl != None and pCrawl.isEndOfWord
  
  
-# driver function
-def main():
-    
-    # -------------------------
-    # TRIE code
-    
-    # Input keys (use only 'a' through 'z' and lower case)
-    keys = ["the","a","there","anaswe","any",
-            "by","their"]
-    output = ["Not present in trie",
-              "Present in trie"]
- 
-    # Trie object
-    t = Trie()
- 
-    # Construct trie
-    for key in keys:
-        t.insert(key)
- 
-    # Search for different keys
-    print("{} ---- {}".format("the",output[t.search("the")]))
-    print("{} ---- {}".format("these",output[t.search("these")]))
-    print("{} ---- {}".format("their",output[t.search("their")]))
-    print("{} ---- {}".format("thaw",output[t.search("thaw")]))
-    
-    
-    # ---------------------
-    # INPUTS for problem
-    
-    # dict of words
-    words_list = ['GET', 'GATE', 'ATE', 'GAG', 'GOT', 'TAG']
-    word_dict = dict()
-    for word in words_list:
-        word_dict[word.lower()] = 1
-    # NxM matrix
-    matrix = list()
-    matrix.append(['G', 'A', 'T'])
-    matrix.append(['A', 'E', 'E'])
-    matrix.append(['G', 'O', 'T'])
-    
-    # --------------------------
-    # SOLUTION -1
-    
+    def print_levels(self):
+        """
+        prints the trie nodes in in level order
+        """
+        queue = [self.root]
+        while queue:
+            current = queue.pop(0)
+            print 'node={}'.format(current.__dict__)
+            if current.children:
+                for child in current.children:
+                    queue.append(child)
+                    
+
+#######################################################
+## SOLUTION -1 
+#######################################################
+
+def matrix_search_simple(matrix, words_dict):
     m = len(matrix)  # no of rows
     n = len(matrix[0])  # no of cols
+    
+    print 'Simple Matrix Search ----'
     
     def matrix_search(is_visited, x, y, letter_list):
         
@@ -172,6 +152,7 @@ def main():
         - Objects of built-in types like (list, set, dict) are mutable. 
         - Custom classes are generally mutable
         """
+        
         letter_list_local = copy.deepcopy(letter_list)
         letter_list_local.append(matrix[x][y])
         
@@ -179,7 +160,7 @@ def main():
         is_visited_local[x][y] = True
         
         word = ''.join(letter_list_local)
-        if word in word_dict.keys():
+        if word in words_dict.keys():
             print 'Found word = {}'.format(word)
             
         if (x-1 >= 0) and (not is_visited_local[x-1][y]):
@@ -208,6 +189,100 @@ def main():
             
             print 'Starting Search from element [{},{}]'.format(i, j)
             matrix_search(is_visited, i, j, list())
+
+#######################################################
+## SOLUTION -2
+#######################################################
+
+def matrix_search_trie(matrix, words_dict):
+    m = len(matrix)  # no of rows
+    n = len(matrix[0])  # no of cols
+    
+    print 'Trie Matrix Search ----'
+    words_trie = Trie()
+    for word in words_dict.keys():
+        words_trie.insert(word.lower())
+    words_trie.print_levels()    
+    
+    def matrix_search(is_visited, x, y, letter_list):
+        
+        letter_list_local = copy.deepcopy(letter_list)
+        letter_list_local.append(matrix[x][y])
+        
+        is_visited_local = copy.deepcopy(is_visited)
+        is_visited_local[x][y] = True
+        
+        word = ''.join(letter_list_local)
+        if word in words_dict.keys():
+            print 'Found word = {}'.format(word)
+            
+        if (x-1 >= 0) and (not is_visited_local[x-1][y]):
+            matrix_search(is_visited_local, x-1, y, letter_list_local)
+            
+        if (x+1 < m) and (not is_visited_local[x+1][y]):
+            matrix_search(is_visited_local, x+1, y, letter_list_local)
+            
+        if (y-1 >= 0) and (not is_visited_local[x][y-1]):
+            matrix_search(is_visited_local, x, y-1, letter_list_local)
+            
+        if (y+1 < n) and (not is_visited_local[x][y+1]):
+            matrix_search(is_visited_local, x, y+1, letter_list_local)
+
+        
+    for i in range(m):
+        for j in range(n):
+            ## to track if the cell in the matrix has been visited            
+            is_visited = [[False for _ in range(n)] for _ in range(m)]
+            
+            print 'Starting Search from element [{},{}]'.format(i, j)
+            matrix_search(is_visited, i, j, list())
+ 
+ 
+ 
+# driver function
+def main():
+    
+    # -------------------------
+    # TRIE code
+    
+    # Input keys (use only 'a' through 'z' and lower case)
+    keys = ["the","a","there","anaswe","any",
+            "by","their"]
+    output = ["Not present in trie",
+              "Present in trie"]
+ 
+    # Trie object
+    t = Trie()
+ 
+    # Construct trie
+    for key in keys:
+        t.insert(key)
+ 
+    # Search for different keys
+    keys = ['the', 'these', 'their', 'thaw', 'ana', 'b']
+    for k in keys:
+        print '{} --> {}'.format(k, t.search(k))
+    
+    
+    
+    # ---------------------
+    # INPUTS for problem
+    
+    # dict of words
+    words_list = ['GET', 'GATE', 'ATE', 'GAG', 'GOT', 'TAG']
+    words_dict = dict()
+    for word in words_list:
+        words_dict[word] = 1
+    # NxM matrix
+    matrix = list()
+    matrix.append(['G', 'A', 'T'])
+    matrix.append(['A', 'E', 'E'])
+    matrix.append(['G', 'O', 'T'])
+    
+    #matrix_search_simple(matrix, words_dict)
+    #matrix_search_trie(matrix, words_dict)
+    
+    
             
 
             
