4b813a62a2298cbbd8cb16f80f51b32e0f5bf168
Amol Kokje <amolkokje@gmail.com>
2018-10-01 09:23:27 -0700

    added special methodss

--- python/python_special_methods.py
+++ python/python_special_methods.py
@@ -1,3 +1,21 @@
+## args, kwargs --> https://pythontips.com/2013/08/04/args-and-kwargs-in-python-explained/
+print '\n --- ARGS, KWARGS --- \n'
+
+def func(arg1, arg2, arg3, arg4, arg5):
+    print 'arg1={}, arg2={}, arg3={}, arg4={}, arg5={}'.format(arg1, arg2, arg3, arg4, arg5)
+    
+# NOTE -> name arg, *args, *kwargs is not important. Only the asterisk *, ** are imp when calling the method    
+# call normally    
+func('a', 'b', 'c', 'd', 'e')
+# call with *args
+func(*('a', 'b','c', 'd', 'e'))
+# call with **kwargs --> NOTE: the key names have to match the variable names of the function
+func(**{'arg1':'a', 'arg2':'b', 'arg3':'c', 'arg4':'d', 'arg5':'e'})
+# call all --> NOTE: When calling all, it is required that you preserve the order
+func('a', *('b','c'), **{'arg4':'d', 'arg5':'e'})
+
+
+
 # REFERENCE: https://docs.python.org/3/reference/datamodel.html
 
 class TestDict(dict):
@@ -6,13 +24,7 @@ class TestDict(dict):
     """
     def __missing__(self, key):
         print '-- missing: key={}'.format(key)
-
-
-class TestClass():
-    def __init__(self, d):
-        print '-- __init__(): constructor'
-        self.d = d
-      
+	
     """
     __setitem__() and __getitem__() allow to access attributes of a class instance as a dict.
     __delitem__() is used to implement deletion
@@ -20,16 +32,22 @@ class TestClass():
     """
     def __getitem__(self, key):
         print '-- __getitem__(): key={}'.format(key)        
-        return self.d[key]  # this will call the __getattribute__()
+        return super(TestDict, self).__getitem__(key)
         
     def __setitem__(self, key, item):
         print '-- __setitem__(): key={}, item={}'.format(key, item)
-        self.d[key] = item  # this will call the __setattr__()
-        
+        super(TestDict, self).__setitem__(key, item)
+		
     def __delitem__(self, key):
         print '-- __delitem__(): key={}'.format(key)
-        del self.d[key]
+        super(TestDict, self).__delitem__(key)
         
+
+
+class TestClass():
+    def __init__(self, d):
+        print '-- __init__(): constructor'
+        self.d = d
         
     """
     __getattribute__() is called when we try to retireve class instance attribute
@@ -53,29 +71,28 @@ class TestClass():
         
         
 if __name__=='__main__':
-    d = {'a':1, 'b':2}
-    tc = TestClass(d) # this will call __setattr__() 
     
-    # __setitem__
-    tc['x'] = 2
-    # __getitem__
-    print "tc['x']={}".format(tc['x'])
-    # __delitem__
-    del tc['x']
-    
-    # get all attributes of class instance as a dict
-    print tc.__dict__
-    
+    print "\n --- CLASS --- \n"
+    d = {'a':1, 'b':2}# this will call __setattr__()
+    tc = TestClass(d)# get all attributes of class instance as a dict
+    print tc.__dict__    
     # __getattribute__
     print tc.d 
     # __getattr__
     print tc.x
+    # __setattr__
+    tc.dummy = 'dummy'
     
+    print "\n --- DICTIONARY ---\n"
     # __missing__
     td = TestDict({'a':1, 'b':2})
     print type(td)
+    # __getitem__
     print td['a']
+	# __missing__
     print td['c']
+    # __setitem__
+    td['c'] = 3 
    
     
     
