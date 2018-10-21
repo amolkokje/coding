e3cdb8fefc4b9d64c21d7bb3775c8b187f436c74
Amol Kokje <amol_kokje@mcafee.com>
2018-10-21 16:20:18 -0700

    more

--- python/other_problems_2.py
+++ python/other_problems_2.py
@@ -395,6 +395,38 @@ def get_node_parent_counts_01(pairs):
     return output
 
 
+# Q: Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand. (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+# You are given a target value to search. If found in the array return its index, otherwise return -1. You may assume no duplicate exists in the array. Your algorithm's runtime complexity must be in the order of O(log n).
+# Example 1:
+# Input: nums = [4,5,6,7,0,1,2], target = 0 --> Output: 4
+# Example 2:
+# Input: nums = [4,5,6,7,0,1,2], target = 3 --> Output: -1
+
+def search_rotated_array(nums, target):
+    if not nums:
+        return -1
+
+    low, high = 0, len(nums) - 1
+
+    while low <= high:
+        mid = (low + high) / 2
+        if target == nums[mid]:
+            return mid
+                
+        if nums[low] <= nums[mid]:
+            # if RIGHT side is not rotated, and the target exists, search there, else the other side
+            if nums[low] <= target <= nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        else:
+            # if RIGHT side is not rotated, and the target exists, search there, else the other side
+            if nums[mid] <= target <= nums[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+    return -1    
    
 if __name__ == '__main__':
     
@@ -523,3 +555,8 @@ if __name__ == '__main__':
     for pair in pairs:
         print '{} and {} have common ancestor = {}'.format(pair[0], pair[1], have_common_ancestor(parent_child_pairs, pair[0], pair[1]))
     
+    
+    print '--------------------------------------------------------------'
+    alist = [ ([4,5,6,7,0,1,2],0), ([4,5,6,7,8,1,2,3],8) ]    
+    for arr in alist:
+        print 'search_rotated_array input={}, output={}'.format(arr, search_rotated_array(arr[0], arr[1]))
