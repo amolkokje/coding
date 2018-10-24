c3000da85429b3888d04e913fa5f597baf78ce04
Amol Kokje <amolkokje@gmail.com>
2018-10-24 08:41:44 -0700

    Update cci_chapter17.py

--- python/cci_chapter17.py
+++ python/cci_chapter17.py
@@ -337,7 +337,7 @@ def find_pairs_sum(arr, sum):
 # 17.4 ->  Oh, no! You have just completed a lengthy document when you have an unfortunate Find/Replace mishap. You have accidentally removed all spaces, punctuation, and capitalization in the document. A sentence like "I reset the computer. It still didn't boot!" would become "iresetthecomputeritstilldidntboot". You figure that you can add back in the punctation and capitalization later, once you get the individual words properly separated. Most of the words will be in a dictionary, but some strings, like proper names, will not.
 # Given a dictionary (a list of words), design an algorithm to find the optimal way of "unconcatenating" a sequence of words. In this case, "optimal" is defined to be the parsing which minimizes the number of unrecognized sequences of characters.
 # For example, the string "jesslookedjustliketimherbrother" would be optimally parsed as "JESS looked just like TIM her brother". This parsing has seven unrecognized characters, which we have capitalized for clarity.
-
+## AMOL - not optimal, maybe incorrect??
 
 
 def add_spaces(olds, word_dictionary):
@@ -366,6 +366,7 @@ def add_spaces(olds, word_dictionary):
                 news.append(word)
                 i += (j-i)
 
+            # AMOL - why checking the same substring again for word_end?    
             word_end = get_word_end(i, j, word_dictionary)
             if word_end:
                 print 'END={} in word={}'.format(word_end, word)
