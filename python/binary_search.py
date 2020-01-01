b271d04e03986bbd28563471c5b05d3e19b61ae4
Amol Kokje <kokjeak@amazon.com>
2019-12-31 16:17:23 -0800

    new

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,35 +1,9 @@
-input_lists = [
-[3,3,3,3,3,3,3,3,3,3,3,3,5,3],
-[5,7,8,9,4,6],
-[2,3,4,5,5,8]
-]
-x = 5
+import sys, os, copy
 
-
-magic_index_input_lists = [
-[-1,0,1,3,8,9],
-[-4,-5,0,2,4],
-
-]
-
-
-
-def test_binarySearch(input_lists, x):
-    print 'testing binarySearch() ---'
-    #for ip_list in input_lists:
-    #    ip_list.sort()
-    #    print 'binarySearch: input={}, searching={}, index={}'.format(ip_list, x, binarySearch(ip_list, x))
-    #    print 'binarySearchRecursive_helper: input={}, searching={}, index={}'.format(ip_list, x, binarySearchRecursive_helper(ip_list, x))
-    #    print 'binarySearch_Last: input={}, searching={}, index={}'.format(ip_list, x, binarySearch_Last(ip_list, x))
-    #    print 'binarySearch_First: input={}, searching={}, index={}'.format(ip_list, x, binarySearch_First(ip_list, x))
-        
-    for ip_list in magic_index_input_lists:
-        print 'binarySearchMagicIndex_helper: input={}, index={}'.format(ip_list, binarySearchMagicIndex_helper(ip_list))
-        
 ############################################################
 # binary search - sorted array
 # works with repeating elements in the list
-        
+
 def binarySearch(input_list, x):
     """
     binary search on a sorted list
@@ -37,53 +11,55 @@ def binarySearch(input_list, x):
     index - when element found
     """
     left = 0
-    right = len(input_list)-1
+    right = len(input_list) - 1
     while left <= right:
-        mid = (left+right)/2
+        mid = (left + right) / 2
         if x > input_list[mid]:
-            left = mid+1
+            left = mid + 1
         elif x < input_list[mid]:
-            right = mid-1
+            right = mid - 1
         else:
             return mid
     return None
-    
+
+
 ############################################################
 # binary search recursive method - sorted array
 # works with repeating elements in the list
 
 def binarySearchRecursive(x, left, right, ip_list):
     if left <= right:
-        mid = (left+right)/2
+        mid = (left + right) / 2
         if x > ip_list[mid]:
-            return binarySearchRecursive(x, mid+1, right, ip_list)
+            return binarySearchRecursive(x, mid + 1, right, ip_list)
         elif x < ip_list[mid]:
-            return binarySearchRecursive(x, left, mid-1, ip_list)
+            return binarySearchRecursive(x, left, mid - 1, ip_list)
         else:
             return mid
-            
+
+
 def binarySearchRecursive_helper(ip_list, x):
-    return binarySearchRecursive(x, 0, len(ip_list)-1, ip_list)
+    return binarySearchRecursive(x, 0, len(ip_list) - 1, ip_list)
 
 
 ############################################################
 # binary search last - sorted array
 # e.g. [3,5,6,7,7,8]. Index of 7 is 4
 # works with repeating elements in the list
-    
+
 def binarySearch_Last(ip_list, x):
     left = 0
-    right = len(ip_list)-1
+    right = len(ip_list) - 1
     last = None
     while left <= right:
-        mid = (left+right)/2
+        mid = (left + right) / 2
         if x > ip_list[mid]:
-            left = mid+1
+            left = mid + 1
         elif x < ip_list[mid]:
-            right = mid-1
+            right = mid - 1
         else:
             last = mid
-            left = mid+1
+            left = mid + 1
     return last
 
 
@@ -94,19 +70,20 @@ def binarySearch_Last(ip_list, x):
 
 def binarySearch_First(ip_list, x):
     left = 0
-    right = len(ip_list)-1
+    right = len(ip_list) - 1
     first = None
     while left <= right:
-        mid = (left+right)/2
+        mid = (left + right) / 2
         if x > ip_list[mid]:
-            left = mid+1
+            left = mid + 1
         elif x < ip_list[mid]:
-            right = mid-1
+            right = mid - 1
         else:
             first = mid
-            right = mid-1
+            right = mid - 1
     return first
-    
+
+
 ############################################################
 # binary search magic index - sorted array
 # i.e. find index in sorted array such that ip_list[i]==i
@@ -114,23 +91,70 @@ def binarySearch_First(ip_list, x):
 # NOTE: tests indicate that this algo goes in infinite loop when list has repeating elements. Example: [3,5,6,7,7,8]
 # this can also be made non-recursive like the implementation of binarySearch()
 
-def binarySearchMagicIndex(ip_list, left, right):
-    while left <= right:
-        mid = (left+right)/2
-        if ip_list[mid] == mid:
-            return mid        
-        elif mid > ip_list[mid]:
-            return binarySearchMagicIndex(ip_list, mid+1, right)
-        else:    
-            return binarySearchMagicIndex(ip_list, left, mid-1)
+# Book: 9.3
+# Q. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
+# FOLLOW UP: What if the values are not distinct?
 
-def binarySearchMagicIndex_helper(ip_list):
-    return binarySearchMagicIndex(ip_list, 0, len(ip_list)-1)
-    
+def binary_search_magic_index_sorted_arr(arr):
+    n = len(arr)
 
-    
+    def _recurse(l, r):
+        if l <= r:
+            m = (l + r) / 2
+            if arr[m] == m:
+                return m
+
+            if m > arr[m]:
+                return _recurse(m + 1, r)
+
+            if m < arr[m]:
+                return _recurse(l, m - 1)
+
+    return _recurse(0, n - 1)
+
+
+# NOTE: if given an array with no repeats, this will scale almost exactly like the previous method
+def binary_search_magic_index_sorted_arr_with_repeats(arr):
+    n = len(arr)
+
+    def _recurse(l, r):
+        if l <= r:
+            m = (l + r) / 2
+            if arr[m] == m:
+                return m
+
+            # search entire left side
+            # DID NOT UNDERSTAND THIS CALCULATION
+            left_max = min(m - 1, arr[m])
+            left = _recurse(l, left_max)
+            if left:
+                return left
+
+            right_min = max(m + 1, arr[m])
+            right = _recurse(right_min, r)
+            return right
+
+    return _recurse(0, n - 1)
+
+
 if __name__ == '__main__':
-    print 'searching x={}'.format(x)    
-    test_binarySearch(input_lists, x)
 
-    
+    # WORKS FOR: sorted arrays, repeats/non-repeats
+    search_for = 5
+    for iparr in [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 5, 3],
+                  [5, 7, 8, 9, 4, 6],
+                  [2, 3, 4, 5, 5, 8],
+                  [8, 3, 4, 5, 5, 2]]:
+        print 'IP: Arr={}, Search={}, Binary Search={}'.format(iparr, search_for, binarySearch(iparr, search_for))
+
+    # WORKS FOR: sorted arrays, no-repeats
+    print '----------------------------------------------------------'
+    for iparr in [[-1, 0, 1, 3, 8, 9], [-4, -5, 0, 2, 4]]:
+        print 'IP={}, Magic Index={}'.format(iparr, binary_search_magic_index_sorted_arr(iparr))
+
+    # WORKS FOR: sorted arrays, repeats/no-repeats
+    print '----------------------------------------------------------'
+    sorted_arr_repeats = [-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13]
+    print 'IP={}, Magic Index with repeats={}'.format(sorted_arr_repeats,
+                                                      binary_search_magic_index_sorted_arr_with_repeats(
+                                                          sorted_arr_repeats))
