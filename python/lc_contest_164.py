3fc4cc36465939d968dc060e6f02be41fdb8bf53
Amol Kokje <kokjeak@amazon.com>
2020-01-08 18:05:08 -0800

    new

---
+++ python/lc_contest_164.py
@@ -0,0 +1,252 @@
+"""
+https://leetcode.com/contest/weekly-contest-164/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps/
+"""
+
+class Solution(object):
+    def numWays(self, steps, arrLen):
+        """
+        :type steps: int
+        :type arrLen: int
+        :rtype: int
+        """
+        #visited = [False for _ in range(arrLen)]
+        self.count = 0
+
+        def _recurse(step_count, place, start):
+            if step_count > steps:
+                return
+
+            if place<0 or place>=arrLen:
+                return
+
+            #print 'place={}, step_count={}'.format(place, step_count)
+            if step_count == steps:
+                if place==start:
+                    self.count += 1
+                    #print 'found, count={}'.format(self.count)
+                    return
+
+            _recurse(step_count+1, place-1, start)
+            _recurse(step_count+1, place+1, start)
+            _recurse(step_count+1, place, start)
+
+        _recurse(0, 0, 0)
+        return self.count
+
+
+
+
+
+"""
+https://leetcode.com/contest/weekly-contest-164/problems/search-suggestions-system/
+"""
+import copy
+
+class TrieNode(object):
+    def __init__(self, x):
+        self.value = x
+        self.child_nodes = list()
+        self.is_word = False
+    def __repr__(self):
+        return '<TN: value={}, word={}>'.format(self.value, self.is_word)
+
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode(None)
+
+    def insert(self, word):
+        current = self.root
+        for w in word:
+            node_found = False
+            for child in current.child_nodes:
+                if child.value == w:
+                    current = child
+                    node_found = True
+                    break
+            if not node_found:
+                new_child = TrieNode(w)
+                current.child_nodes.append(new_child)
+                current = new_child
+        # at the last node
+        current.is_word = True
+
+    def print_all(self):
+        current = self.root
+
+        def _recurse(node, word):
+            if node:
+                word_local = copy.deepcopy(word)
+                word_local += node.value
+                if node.is_word:
+                    print 'WORD={}'.format(word_local)
+
+                if node.child_nodes:
+                    for child in node.child_nodes:
+                        _recurse(child, word_local)
+
+        for child in current.child_nodes:
+            _recurse(child, child.value)
+
+    def search(self, prefix):
+        current = self.root
+        output = list()
+
+        # go till the prefix last char node
+        for p in prefix:
+            node_found = False
+            for child in current.child_nodes:
+                if child.value == p:
+                    current = child
+                    node_found = True
+                    break
+            if not node_found:
+                print 'Prefix {} not found!'.format(prefix)
+                return
+
+        # now current is at prefix last char
+        # find all strings possible with this prefix
+
+        def _recurse(post_formed, node):
+            if node:
+                post_formed += node.value
+                if node.is_word:
+                    print '--> word={}'.format(prefix + post_formed)
+                    output.append(prefix + post_formed)
+
+                pf_local = copy.deepcopy(post_formed)
+                for child in node.child_nodes:
+                    _recurse(pf_local, child)
+
+        print current
+        _recurse('', current)
+        return output
+
+
+class Solution(object):
+    def suggestedProducts(self, products, searchWord):
+        """
+        :type products: List[str]
+        :type searchWord: str
+        :rtype: List[List[str]]
+        """
+
+        trie = Trie()
+        for p in products:
+            print 'inserting {}'.format(p)
+            trie.insert(p)
+
+        trie.print_all()
+        return
+
+        word = ''
+        for w in searchWord:
+            word += w
+            print 'word={}'.format(word)
+            print 'search output = {}'.format(trie.search(word))
+
+"""
+https://leetcode.com/contest/weekly-contest-164/problems/count-servers-that-communicate/
+"""
+class Solution(object):
+    def countServers(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m = len(grid)  # rows
+        n = len(grid[0]) # cols
+
+        #print 'm={}, n={}'.format(m,n)
+
+        #row_servers = [ 0 for _ in range(m) ]
+        #col_servers = [ 0 for _ in range(n) ]
+
+        #for i in range(m):
+        #    row_servers[i] = len( filter(lambda x:x==1, grid[i]) )
+
+        #for i in range(n):
+        #    col_servers[i] = len( filter(lambda x:x==1, [ grid[k][i] for k in range(m) ]) )
+
+        comm_count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    print '---> ({},{})'.format(i,j)
+
+                    """
+                    r_before = grid[i][:j]
+                    #print 'r_before={}'.format(r_before)
+                    if 1 in r_before:
+                        comm_count += 1
+                    """
+
+                    r_after = grid[i][j+1:]
+                    #print 'r_after={}'.format(r_after)
+                    if 1 in r_after:
+                        comm_count += 1
+
+                    """
+                    col_before = [ grid[k][j] for k in range(i) ] ###
+                    #print 'col_before={}'.format(col_before)
+                    if 1 in col_before:
+                        comm_count += 1
+                    """
+
+                    col_after = [ grid[k][j] for k in range(i+1,m) ]
+                    #print 'col_after={}'.format(col_after)  ## ??
+                    if 1 in col_after:
+                        comm_count += 1
+
+                print 'i={},j={}, count={}'.format(i,j,comm_count)
+        return comm_count
+
+
+
+"""
+https://leetcode.com/contest/weekly-contest-164/problems/minimum-time-visiting-all-points/
+"""
+
+class Solution(object):
+    def minTimeToVisitAllPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        n = len(points)
+        #visited = [ False for _ in range(n) ]
+
+        current = points[0]
+        steps = 0
+        for point in points[1:]:
+            #print 'trynig to reach {}'.format(point)
+
+            while True:
+                #print 'current: {}'.format(current)
+
+                if current == point:
+                    #steps += 1
+                    #print '--> reached: {}'.format(current)
+                    break
+
+                if current[0] < point[0]:
+                    next_x = current[0]+1
+                elif current[0] > point[0]:
+                    next_x = current[0]-1
+                else:
+                    next_x = current[0]
+
+                if current[1] < point[1]:
+                    next_y = current[1]+1
+                elif current[1] > point[1]:
+                    next_y = current[1]-1
+                else:
+                    next_y = current[1]
+
+                current = [next_x, next_y]
+                steps += 1
+
+        return steps
+
+
+
+        
---
+++ python/lc_contest_165.py
@@ -0,0 +1,134 @@
+"""
+https://leetcode.com/contest/weekly-contest-165/problems/count-square-submatrices-with-all-ones/
+"""
+
+class Solution(object):
+
+    def is_all_one(self, square):
+        #print square
+        n = len(square)
+        for i in range(n):
+            for j in range(n):
+                #print i,j
+                if square[i][j] != 1:
+                    return False
+        return True
+
+    def countSquares(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+        m = len(matrix) # rows
+        n = len(matrix[0]) # cols
+        print 'm={}, n={}'.format(m,n)
+
+        count = 0
+        max_sides = min(m,n)
+        for side in range(1, max_sides+1):
+            # in a row
+            for i in range(m-side+1):
+                # in a col
+                for j in range(n-side+1):
+
+                    # generate square
+                    square = list()
+                    for x in range(i,i+side):
+                        square.append(matrix[x][j:j+side])
+
+                    if self.is_all_one(square):
+                        count += 1
+                        #print '** count={}'.format(count)
+
+        return count
+
+
+"""
+https://leetcode.com/contest/weekly-contest-165/problems/find-winner-on-a-tic-tac-toe-game/
+"""
+
+class Solution(object):
+
+    def has_winner(self, board):
+        n = len(board)
+
+        def _all_same(arr):
+            xor = reduce(lambda x,y:x^y, arr)
+            return True if xor==0 else False
+
+        for i in range(n):
+            if _all_same(board[i]) or _all_same([board[j][i] for j in range(n)]):
+                return True
+
+        if _all_same([board[i][i] for i in range(n)]) or _all_same([board[i][n-1-i] for i in range(n)]):
+            return True
+
+        return False
+
+
+
+
+    def tictactoe(self, moves):
+        """
+        :type moves: List[List[int]]
+        :rtype: str
+        """
+        n = 3
+        max_moves = n*n
+        moves_a = [ [ False for _ in range(n) ] for _ in range(n) ]
+        moves_b = [ [ False for _ in range(n) ] for _ in range(n) ]
+
+        c = 0
+        total_moves = len(moves)
+        for c in range(total_moves):
+            move = moves[c]
+            if c%2!=0:
+                moves_b[move[0]][move[1]] = True
+                if self.has_winner(moves_b):
+                    return 'B'
+            else:
+                moves_a[move[0]][move[1]] = True
+                if self.has_winner(moves_a):
+                    return 'A'
+            c += 1
+
+        if c < max_moves-1:
+            return 'Pending'
+        return 'Draw'
+
+
+"""
+https://leetcode.com/contest/weekly-contest-165/problems/number-of-burgers-with-no-waste-of-ingredients/
+"""
+
+class Solution(object):
+    def numOfBurgers(self, tomatoSlices, cheeseSlices):
+        """
+        :type tomatoSlices: int
+        :type cheeseSlices: int
+        :rtype: List[int]
+        """
+        possibilities = list()
+
+        def _recurse(total_jumbo, total_small, tomato, cheese):
+            #print 't={}, c={}'.format(tomato, cheese)
+
+            if tomato==0 and cheese==0:
+                ele = [total_jumbo, total_small]
+                if ele not in possibilities:
+                    possibilities.append(ele)
+
+            if tomato<2 and cheese<1:
+                return
+
+            if tomato>=2 and cheese>=1:
+                _recurse(total_jumbo, total_small+1, tomato-2, cheese-1)
+
+            if tomato>=4 and cheese>=1:
+                _recurse(total_jumbo+1, total_small, tomato-4, cheese-1)
+
+
+        _recurse(0, 0, tomatoSlices, cheeseSlices)
+        return possibilities[0]
+
+
