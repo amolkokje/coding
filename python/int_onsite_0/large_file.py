dccbb29053b40409e656747c724e2a0450eeb962
Amol Kokje <kokjeak@amazon.com>
2020-02-04 15:06:39 -0800

    new

--- python/int_onsite_0/large_file.py
+++ python/int_onsite_0/large_file.py
@@ -1,5 +1,6 @@
 import sys, os
 
+
 # NEEDED ONLY TO GENERATE THE INPUT
 def create_big_file(filename, one_large_sentence):
     sentence = 'My name is Amol Kokje. '
@@ -7,7 +8,7 @@ def create_big_file(filename, one_large_sentence):
     with open(filename, 'w') as fh:
         fh.write(sentence)
 
-    for _ in range(10000):
+    for _ in range(10 ** 5):
         with open(filename, 'a') as fh:
             if one_large_sentence:
                 fh.write(sentence)
@@ -19,12 +20,25 @@ def process_file(input_filename, replace_str, replace_words, output_filename):
     with open(output_filename, 'w') as outputfh:
         with open(input_filename) as inputfh:
             # process chunk by chunk
+            chunk_buffer = ''
             for chunk in read_in_chunks(inputfh, 128):
-                new_chunk = process_chunk(chunk, replace_str, replace_words)
-                outputfh.write(new_chunk)
+                # if the last byte is a space, period or comma, process the chunk_buffer
+                # if not, add to buffer
+                ##print 'chunk=[{}]'.format(chunk)
+                chunk_buffer += chunk
+                if chunk[-1] in [' ', '.', ',', ':']:
+                    new_chunk_buffer = process_chunk(chunk_buffer, replace_str, replace_words)
+                    outputfh.write(new_chunk_buffer)
+                    ##print '--> DUMP: chunk_buffer=[{}]'.format(chunk_buffer)
+                    chunk_buffer = ''
+
+            # dump the last buffer
+            new_chunk_buffer = process_chunk(chunk_buffer, replace_str, replace_words)
+            outputfh.write(new_chunk_buffer)
 
 
 def read_in_chunks(fh, chunk_size):
+    # LIMITATION - word breaks over chunks
     while True:
         # read file by specified chunk size
         chunk = fh.read(chunk_size)
@@ -36,23 +50,20 @@ def read_in_chunks(fh, chunk_size):
 
 
 def process_chunk(chunk, replace_str, replace_words):
-    # handle: . , :
-    new_chunk = ''
-    for word in chu
     for word in replace_words:
         chunk = chunk.replace(word, replace_str)
     return chunk
 
 
 if __name__ == '__main__':
-    input_filename = 'input_file.log'
+    input_filename = 'input_file_newline.log'
     output_filename = 'output_file.log'
 
     input_words_file = 'input_words.txt'
     replace_str = '*'
 
     # GENERATE INPUT LOG FILE
-    create_big_file(input_filename, False)
+    # create_big_file(input_filename, True)
 
     # read the words to replace
     replace_words = None
@@ -63,7 +74,6 @@ if __name__ == '__main__':
     process_file(input_filename, replace_str, replace_words, output_filename)
 
     # REMOVE LOG FILES
-    log_files = filter(lambda x:x.endswith('.log'), os.listdir(os.getcwd()))
-    for file in log_files:
-        os.remove(file)
-
+    # log_files = filter(lambda x:x.endswith('.log'), os.listdir(os.getcwd()))
+    # for file in log_files:
+    #    os.remove(file)
